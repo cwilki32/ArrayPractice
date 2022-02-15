439202c539a0682a4e2db59023b75befa0e8b229
Collin Wilkins <cwilki32@Collins-MBP.attlocal.net>
2022-02-15 12:50:21 -0500

    Exercise 7.1 changes to include daily print and average

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -10,15 +12,23 @@ public class Main {
         // Initialize your project as a git repository and add/commit all changes as you work
         // Declare an array named dailyAverageTemp
         // Instruct the user to input daily average temperatures (in F) for 7 days
-        // Each time the user inputs a value, add it to your dailyAverageTemp array
+        // Commit 1 Each time the user inputs a value, add it to your dailyAverageTemp array
+        // Commit 2 Write a program that loops through your array and prints out each daily average temp
+        // Commit 3 Write a program that prints out the weekly average temperature
 
 
         double [] dailyAverageTemp = new double[7];
         Scanner userInput = new Scanner(System.in);
+        int temp, sum = 0;
+        float average;
 
         for(int i = 0; i < dailyAverageTemp.length; i++) {
             System.out.println("Please input average temperature in Fahrenheit for day " + (i + 1) + ": ");
-            dailyAverageTemp[i] = userInput.nextDouble();
+            dailyAverageTemp[i] = userInput.nextInt();
+            sum += dailyAverageTemp[i];
         }
+        average = (float)sum/7;
+        System.out.println("The temperatures inputed were as follows: " + Arrays.toString(dailyAverageTemp));
+        System.out.println("The average for the week is: " + average + " degrees.");
     }
 }
